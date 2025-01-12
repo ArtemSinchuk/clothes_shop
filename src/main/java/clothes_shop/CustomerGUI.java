24e962541721863118c65503925bf1e819825284
Artem Sinchuk <158162880+ArtemSinchuk@users.noreply.github.com>
2025-01-12 16:58:48 +0100

    bug fix + combo boxes update

--- src/main/java/clothes_shop/CustomerGUI.java
+++ src/main/java/clothes_shop/CustomerGUI.java
@@ -1,16 +1,20 @@
-package clothes_shop;
+package clothes_shop.gui;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import clothes_shop.Customer;
 
 public class CustomerGUI extends JFrame {
   private Customer customer;
   private JPanel pnlMainPanel;
   private JComboBox<String> comboType, comboSize, comboCost;
   private JLabel lblBalance;
+  private String[] types;
+  private String[] sizes;
+  private double[] costs;
 
     public CustomerGUI() {
       initialization();
@@ -20,11 +24,21 @@ public class CustomerGUI extends JFrame {
     }
 
     private void initialization() {
-      comboType = new JComboBox<>();
-      comboSize = new JComboBox<>();
+      types = new String[]{"-", "Shirt"};
+      sizes = new String[]{"-", "XS", "S", "M", "L", "XL"};
+      costs = new double[]{};
+      customer = new Customer();
+      comboType = new JComboBox<>(types);
+      comboSize = new JComboBox<>(sizes);
       comboCost = new JComboBox<>();
       pnlMainPanel = new JPanel();
       lblBalance = new JLabel(customer.toString());
+    }
+
+    public void configureComboBox() {
+      comboType.setLocation(150, 200);
+      comboSize.setLocation(150, 250);
+      comboCost.setLocation(150, 300);
     }
 
     public void addComponents() {
@@ -32,21 +46,17 @@ public class CustomerGUI extends JFrame {
       pnlMainPanel.add(comboType);
       pnlMainPanel.add(comboSize);
       pnlMainPanel.add(comboCost);
+      pnlMainPanel.add(lblBalance);
     }
 
     public void configureFrame() {
       setTitle("Customer");
-      setBounds(500, 500, 500, 700);
+      setSize(500, 700);
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       setLocationRelativeTo(null);
       setVisible(true);
     }
-
-    public void configureComboBox() {
-      comboType.setLocation(150, 200);
-    }
-
     public static void main(String[] args) {
-      SwingUtilities.invokeLater(() -> new Customer());
+      SwingUtilities.invokeLater(() -> new CustomerGUI());
     }
 }
