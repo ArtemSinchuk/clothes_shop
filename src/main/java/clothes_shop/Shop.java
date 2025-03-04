b1cbfea6e5c31895e69cae77f0674aba3cdccff3
Artem Sinchuk <158162880+ArtemSinchuk@users.noreply.github.com>
2025-03-04 12:15:55 +0100

    Update Shop.java

--- src/main/java/clothes_shop/Shop.java
+++ src/main/java/clothes_shop/Shop.java
@@ -1,29 +1,28 @@
 package clothes_shop;
 
-import java.util.Arrays;
 import java.util.Random;
+import clothes_shop.exceptions.ShopCapacityExceedException;
 
 public class Shop {
 
   private Clothing[] clothes;
   private int totalCashDesk;
   private int freeCashDesk;
-  private int nextIndex;
+  private int currentCapacityInArray;
   private Random random = new Random();
-
 
   public Shop() {
     this.clothes = new Clothing[100];
     this.totalCashDesk = 10;
-    this.freeCashDesk = getRandomCashDesk();
-    this.nextIndex = 0;
+    this.freeCashDesk = getFreeCashDesk();
+    this.currentCapacityInArray = 0;
   }
 
   public Shop(int capacity) {
       this.clothes = new Clothing[capacity];
       this.totalCashDesk = 10;
-      this.freeCashDesk = getRandomCashDesk();
-      this.nextIndex = 0;
+      this.freeCashDesk = getFreeCashDesk();
+      this.currentCapacityInArray = 0;
   }
 
   public Clothing getClothing(int index) {
@@ -35,8 +34,8 @@ public class Shop {
     }
       return clothes[index];
   }
-  
-  public int clothesLength() {
+
+  public int getClothesLength() {
     return clothes.length;
   }
 
@@ -49,28 +48,38 @@ public class Shop {
   }
 
   public int getFreeCashDesk() {
-      return freeCashDesk;
-  }
-
-  public int getRandomCashDesk() {
     return random.nextInt(totalCashDesk) + 1;
   }
-
+  
   public void setFreeCashDesk(int freeCashDesk) {
       this.freeCashDesk = freeCashDesk;
   }
 
-  public int getNextIndex() {
-    return nextIndex;
+  public int getCurrentCapacityInArray() {
+    return currentCapacityInArray;
   }
 
-  public void addClothing(Clothing clothing) {
-    if (nextIndex < clothes.length) {
-        clothes[nextIndex] = clothing;
-        nextIndex++;
+  public void addClothing(Clothing clothes2) {
+    if (currentCapacityInArray < clothes.length) {
+        clothes[currentCapacityInArray] = clothes2;
+        currentCapacityInArray++;
     } else {
-        System.out.printf("No space to add more clothing. %d/%d", nextIndex, clothes.length);
+      throw new ShopCapacityExceedException(
+        String.format("No space to add more clothing. %d/%d", currentCapacityInArray, clothes.length)
+        );
     }
   }
-}
 
+  public void addClothing(Clothing[] clothing) {
+    for (int i = 0; i < clothing.length; i++) {
+      if (currentCapacityInArray < clothes.length) {
+        clothes[currentCapacityInArray] = clothing[i];
+        currentCapacityInArray++;
+      } else {
+        throw new ShopCapacityExceedException(
+          String.format("No space to add more clothing. %d/%d", currentCapacityInArray, clothes.length)
+          );
+        }
+      }
+  }
+}
